3334585ac02476ec0480977b9e87aec00a5dfe29
Mekni_Wassime <60438665+MekniWassime@users.noreply.github.com>
2022-05-29 15:18:31 +0100

    Create auth_service.py

---
+++ authentification/auth_service.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_restful import reqparse
+from pymongo import MongoClient
+
+client = MongoClient('localhost:27017')
+
+uri = "mongodb://localhost:27017/"
+client = MongoClient(uri)
+db = client.get_database("lab3-microservices")
+users = db.get_collection("users")
+
+app = Flask(__name__)
+api = Api(app)
+parser = reqparse.RequestParser()
+parser.add_argument('username', type=str, help='User name is required', required=True)
+parser.add_argument('password', type=str, help='User password is required', required=True)
+
+class AuthService(Resource):
+    def post(self):
+        args = parser.parse_args()
+        user = users.find_one({'username': args['username']})
+        if(user):
+            if(args['password']==user['password']):
+                return {"username": user['username']}
+            else:
+                return "Wrong password", 401
+        else:
+            return "User not found", 401
+
+api.add_resource(AuthService, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
