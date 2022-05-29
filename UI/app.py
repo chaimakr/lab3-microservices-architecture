ecdd1552841a93103232a94d25de1312e446f734
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-05-29 21:06:52 +0100

    add product info display

--- UI/app.py
+++ UI/app.py
@@ -1,5 +1,7 @@
 from email.policy import default
+from functools import reduce
 import json
+from datetime import datetime as dt
 from flask import Flask, render_template , url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
@@ -22,6 +24,14 @@ sess = Session(app)
 
 #     def __repr__(self) -> str:
 #         return '<product %p>' % self.id 
+
+def greater_date(date1, date2):
+    a = dt.strptime(date1, "%Y/%m/%d")
+    b = dt.strptime(date2, "%Y/%m/%d")
+    if a > b:
+        return date1
+    else:
+        return date2
 
 @app.route("/")
 def index():
@@ -62,6 +72,23 @@ def products_get():
         data = json.loads(res.text)
         products = json.loads(data["data"])
         return render_template('product.html', products=products)
+    else:
+        return render_template('login.html')
+
+@app.route("/products/<product_code>", methods=['GET'])
+def products_get_product(product_code):
+    if('username' in session):
+        res = requests.get('http://localhost:5002/products/'+product_code)
+        data = json.loads(res.text)
+        products = json.loads(data["data"])
+        print(products)
+        product = {}
+        product["Product_Code"] = products[0]["Product_Code"]
+        product["Warehouse"] = products[0]["Warehouse"]
+        product["Product_Category"] = products[0]["Product_Category"]
+        product["Latest_Date"] = reduce(greater_date, [product["Date"] for product in products])
+        product["Order_Demand_Total"] = sum(int(product["Order_Demand"]) for product in products)
+        return render_template('product-info.html', product=product)
     else:
         return render_template('login.html')
 
